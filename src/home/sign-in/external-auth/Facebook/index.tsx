4115b32397ee4a34bd5129f35f154b16031e19d6
Alex Toryanik <alex1toryanik@gmail.com>
2019-10-29 00:31:19 +0200

    feature: add login with mock data in Facebook/GoogleLogin components

--- src/home/sign-in/external-auth/Facebook/index.tsx
+++ src/home/sign-in/external-auth/Facebook/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import styles from '../../styles.module.scss';
+import { Token } from '../types';
+import authApi from '../api';
+import useLocalStorage from 'react-use-localstorage';
 
 const LoginFacebook = () => {
+  const [, setAccessToken] = useLocalStorage('accessToken', '');
+  const [, setRefreshToken] = useLocalStorage('refreshToken', '');
+
+  const setTokens = (token: Token) => {
+    setAccessToken(token.accessToken);
+    setRefreshToken(token.refreshToken);
+  };
+
+  const login = () => {
+    authApi.signIn().then(token => setTokens(token));
+  };
+
   return (
-    <button className={`${styles.start} button is-rounded is-large is-facebook is-full-width`}>
+    <button
+      className={`${styles.start} button is-rounded is-large is-facebook is-full-width`}
+      onClick={login}
+    >
       <span className="icon">
         <i className="fab fa-facebook-f is-white"></i>
       </span>
--- src/home/sign-in/external-auth/Google/index.tsx
+++ src/home/sign-in/external-auth/Google/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import styles from '../../styles.module.scss';
+import { Token } from '../types';
+import authApi from '../api';
+import useLocalStorage from 'react-use-localstorage';
 
 const LoginGoogle = () => {
+  const [, setAccessToken] = useLocalStorage('accessToken', '');
+  const [, setRefreshToken] = useLocalStorage('refreshToken', '');
+
+  const setTokens = (token: Token) => {
+    setAccessToken(token.accessToken);
+    setRefreshToken(token.refreshToken);
+  };
+
+  const login = () => {
+    authApi.signIn().then(token => setTokens(token));
+  };
+
   return (
-    <button className={`${styles.start} button is-rounded is-large is-google is-full-width `}>
+    <button
+      className={`${styles.start} button is-rounded is-large is-google is-full-width `}
+      onClick={login}
+    >
       <div className="columns is-mobile">
         <div className="column">
           <span className="icon">
