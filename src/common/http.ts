67c78d4007dc4be5788e40c9991886f00444569a
Alex Toryanik <alex1toryanik@gmail.com>
2019-10-29 00:24:54 +0200

    build: change default api route

--- src/common/http.ts
+++ src/common/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import isProduction from './utils/is-production';
 
 const httpClient = axios.create({
-  baseURL: isProduction ? 'https://example.com/api' : 'http://localhost:5000',
+  baseURL: isProduction ? 'https://example.com/api' : 'http://localhost:8080',
 });
 
 export default httpClient;
