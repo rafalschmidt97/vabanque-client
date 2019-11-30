9b0725b4b035884fa4fec6a6899b4b385cf2fa27
Alex Toryanik <alex1toryanik@gmail.com>
2019-11-30 16:17:12 +0200

    feature: add user-settings view

---
+++ src/user-settings/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import FooterMenu from '../common/component/footer-menu';
+import styles from './styles.module.scss';
+
+const UserSettings = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Settings</title>
+      </Helmet>
+      <section className="hero is-primary is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <div className="container has-padding-bottom-75">
+              <button className={`${styles.big} button is-large is-info  is-fullwidth is-rounded`}>
+                Update Profile
+              </button>
+            </div>
+            <div className="container has-margin-top-75">
+              <button
+                className={`${styles.big} button is-large is-danger  is-fullwidth is-rounded`}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+        <FooterMenu />
+      </section>
+    </>
+  );
+};
+
+export default UserSettings;
