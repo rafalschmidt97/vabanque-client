d84befbcffaada3a9deebe5ed2116f10dd26380c
Alex Toryanik <alex1toryanik@gmail.com>
2019-12-10 17:59:09 +0200

    feature: socket middleware

--- src/common/middleware/socket.ts
+++ src/common/middleware/socket.ts
@@ -1,24 +1,21 @@
-import {
-  SocketDisconnected,
-  SocketActionTypes,
-  SocketConnected,
-} from './../../core/socket/state/actions';
+import { Disconnect } from './../../core/game/state/actions';
+import { Disconnected, SocketActionTypes, Connected } from './../../core/socket/state/actions';
 import { Middleware, Store } from 'redux';
 import SockJS from 'sockjs-client';
 import { store } from '../../app';
 import { AuthActionTypes } from '../../core/auth/state/actions';
 import { RootState } from '../../core/state';
-import socketService from '../util/socket-service';
+import socketService from '../../core/socket/socket-service';
 import AppConstants from '../constants';
 
 let socket: WebSocket | null = null;
 const onOpen = (store: Store<RootState>) => () => {
-  store.dispatch(new SocketConnected());
+  store.dispatch(new Connected());
 };
 
 const onClose = (store: Store<RootState>) => (error: CloseEvent) => {
   console.log(error);
-  store.dispatch(new SocketDisconnected());
+  store.dispatch(new Disconnected());
 };
 
 function sendAction(type: string, payload: any) {
@@ -55,14 +52,42 @@ const webSocket: Middleware = () => next => action => {
         ],
       });
       break;
+    case SocketActionTypes.Create:
+      sendAction('create', {
+        duration: action.payload.duration,
+        entry: action.payload.entry,
+        progression: action.payload.progression,
+      });
+      break;
     case SocketActionTypes.Join:
       sendAction('join', { code: action.payload });
       break;
-    case SocketActionTypes.Create:
-      sendAction('create', { code: action.payload });
-      break;
     case SocketActionTypes.Start:
-      sendAction('start', { code: action.payload });
+      sendAction('start', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Pause:
+      sendAction('pause', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Resume:
+      sendAction('resume', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Leave:
+      sendAction('leave', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Sync:
+      sendAction('sync', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Raise:
+      sendAction('raise', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Remove:
+      sendAction('remove', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Finish:
+      sendAction('finish', { gameId: action.payload });
+      break;
+    case SocketActionTypes.Rank:
+      sendAction('rank', { gameId: action.payload });
       break;
     case SocketActionTypes.Disconnect:
       if (socket !== null) {
@@ -70,6 +95,9 @@ const webSocket: Middleware = () => next => action => {
       }
       socket = null;
       console.log('websocket closed');
+      break;
+    case SocketActionTypes.Disconnected:
+      store.dispatch(new Disconnect());
       break;
   }
   next({ ...action });
