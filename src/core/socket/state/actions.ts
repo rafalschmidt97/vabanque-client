44d48b63d177022883a61af530bc248cc0729897
Alex Toryanik <alex1toryanik@gmail.com>
2019-12-10 17:58:24 +0200

    feature: create socket actions

--- src/core/socket/state/actions.ts
+++ src/core/socket/state/actions.ts
@@ -1,24 +1,31 @@
-import { GameSettings, code, gameId, accessToken } from './types';
+import { GameSettings, code as Code, gameId as GameId, accessToken, Ranks } from './types';
 import { Action } from 'redux';
 
 export enum SocketActionTypes {
   Connect = '[Socket] Connect',
-  Join = '[Socket] Join',
-  Create = '[Socket] Create',
-  Start = '[Socket] Start',
   Connected = '[Socket] Connected',
+  Create = '[Socket] Create',
+  Join = '[Socket] Join',
+  Start = '[Socket] Start',
+  Pause = '[Socket] Pause',
+  Resume = '[Socket] Resume',
+  Leave = '[Socket] Leave',
+  Sync = '[Socket] Sync',
+  Raise = '[Socket] Raise',
+  Remove = '[Socket] Remove',
+  Finish = '[Socket] Finish',
+  Rank = '[Socket] Rank',
   Disconnect = '[Socket] Disconnect',
   Disconnected = '[Socket] Disconnected',
 }
 
-export class SocketConnect implements Action {
+export class Connect implements Action {
   readonly type = SocketActionTypes.Connect;
   constructor(public payload: accessToken) {}
 }
 
-export class SocketJoin implements Action {
-  readonly type = SocketActionTypes.Join;
-  constructor(public payload: code) {}
+export class Connected implements Action {
+  readonly type = SocketActionTypes.Connected;
 }
 
 export class Create implements Action {
@@ -26,26 +33,60 @@ export class Create implements Action {
   constructor(public payload: GameSettings) {}
 }
 
+export class Join implements Action {
+  readonly type = SocketActionTypes.Join;
+  constructor(public payload: Code) {}
+}
+
 export class Start implements Action {
   readonly type = SocketActionTypes.Start;
-  constructor(public payload: gameId) {}
+  constructor(public payload: GameId) {}
 }
 
-export class SocketConnected implements Action {
-  readonly type = SocketActionTypes.Connected;
+export class Pause implements Action {
+  readonly type = SocketActionTypes.Pause;
+  constructor(public payload: GameId) {}
 }
 
-export class SocketDisconnect implements Action {
+export class Resume implements Action {
+  readonly type = SocketActionTypes.Resume;
+  constructor(public payload: GameId) {}
+}
+
+export class Leave implements Action {
+  readonly type = SocketActionTypes.Leave;
+  constructor(public payload: GameId) {}
+}
+
+export class Sync implements Action {
+  readonly type = SocketActionTypes.Sync;
+  constructor(public payload: GameId) {}
+}
+
+export class Raise implements Action {
+  readonly type = SocketActionTypes.Raise;
+  constructor(public payload: GameId) {}
+}
+
+export class Remove implements Action {
+  readonly type = SocketActionTypes.Remove;
+  constructor(public payload: GameId) {}
+}
+
+export class Finish implements Action {
+  readonly type = SocketActionTypes.Finish;
+  constructor(public payload: GameId) {}
+}
+
+export class Rank implements Action {
+  readonly type = SocketActionTypes.Rank;
+  constructor(public payload: Ranks) {}
+}
+
+export class Disconnect implements Action {
   readonly type = SocketActionTypes.Disconnect;
 }
 
-export class SocketDisconnected implements Action {
+export class Disconnected implements Action {
   readonly type = SocketActionTypes.Disconnected;
 }
-
-export type SocketActions =
-  | SocketConnect
-  | SocketJoin
-  | SocketConnected
-  | SocketDisconnect
-  | SocketDisconnected;
--- src/core/socket/state/types.ts
+++ src/core/socket/state/types.ts
@@ -11,3 +11,30 @@ export interface GameSettings {
   entry: string;
   progression: Progression[];
 }
+
+export interface Ranks {
+  gameId: gameId;
+  rankedAccountsIds: number[];
+}
+
+export enum SocketResponse {
+  Created = 'created',
+  PlayerJoined = 'player_joined',
+  Joined = 'joined',
+  Started = 'started',
+  Paused = 'paused',
+  Resumed = 'resumed',
+  PlayerLeft = 'player_left',
+  Left = 'left',
+  Sync = 'sync',
+  Raised = 'raised',
+  PlayerRemoved = 'player_removed',
+  Removed = 'removed',
+  FinishedWait = 'finished_wait',
+  Finished = 'finished',
+  Ranked = 'ranked',
+  PlayerReconnected = 'player_reconnected',
+  PlayerDisconnected = 'player_disconnected',
+  Error = 'error',
+  JoinFailed = 'GAME_JOIN_FAILED',
+}
